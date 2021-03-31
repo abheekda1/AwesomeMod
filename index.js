ad0aa2575e1da7a2c81dd7b9e96a16b7e23a9cb3
Abheek Dhawan <abheekd@protonmail.com>
2021-03-31 14:10:21 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+
+client.on("ready", () => {
+  console.log("Logged in as " + client.user.tag + "!");
+});
+
+client.on("guildMemberAdd", member => {
+  const compRole = member.guild.roles.cache.get("826846965114339419");
+  const botRole = member.guild.roles.cache.get("826871012724441158");
+  if (member.bot) {
+    member.roles.add(botRole).catch(console.error);
+  } else {
+    member.roles.add(compRole).catch(console.error);
+  }
+});
+
+client.on('messageDelete', message => {
+  const deleteEmbed = new Discord.MessageEmbed()
+    .setTitle('Message Deleted')
+    .addField('Author', message.author.tag)
+    .addField('Message', message.content)
+    .setThumbnail(message.author.avatarURL())
+    .setFooter("ID: " + message.id)
+    .setTimestamp()
+    .setColor('ff0000');
+  message.guild.channels.cache.get("826876551756513314").send(deleteEmbed).catch(console.error);
+});
+
+client.on('messageUpdate', (originalMessage, editedMessage) => {
+  if (editedMessage.author.bot) {
+    return;
+  }
+  const editEmbed = new Discord.MessageEmbed()
+    .setTitle("Message Edited")
+    .addField("Author", editedMessage.author.tag)
+    .addField("Message", `<${originalMessage}\n>${editedMessage}`)
+    .setThumbnail(editedMessage.author.avatarURL())
+    .setFooter("ID: " + editedMessage.id)
+    .setTimestamp()
+    .setColor('004444');
+    editedMessage.guild.channels.cache.get("826876551756513314").send(editEmbed).catch(console.error);
+});
+
+client.login(process.env.BOT_TOKEN);
