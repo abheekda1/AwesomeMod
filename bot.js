1a698d5b594ba2110b0f8349319ac3c879471125
Abheek Dhawan <abheekd@protonmail.com>
2021-04-04 21:12:31 -0500

    Add channel to message delete logs

--- bot.js
+++ bot.js
@@ -668,6 +668,7 @@ client.on('messageDelete', message => {
     .setAuthor(message.author ? message.author.tag : "Unknown", message.author ? message.author.avatarURL() : client.user.defaultAvatarURL)
     .addField('Author', message.author ? message.author.tag : "Message not cached")
     .addField('Message', message.content ? message.content : "Message not cached")
+    .addField('Channel', message.channel ? message.channel : "Message not cached")
     .setThumbnail(message.author.avatarURL())
     .setFooter("ID: " + message.id)
     .setTimestamp()
